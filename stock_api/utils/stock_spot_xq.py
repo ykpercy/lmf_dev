5ae251a2914301d55b4520acbfe93b2e5d811a35
ykpercy <ykpercy@qq.com>
2025-01-03 17:03:14 +0800

    fix token

--- stock_api/utils/stock_spot_xq.py
+++ stock_api/utils/stock_spot_xq.py
@@ -44,7 +44,7 @@ def stock_individual_spot_xq(
     """
     session = requests.Session()
     headers = {
-        "cookie": "xq_a_token=7b83b758d4c2507f2ce3112dd74e4c9b667fb4ed;",
+        "cookie": "xq_a_token=59ea2271a885e3055f8373265dd34d660fd48bf2;",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 "
         "(KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
     }
